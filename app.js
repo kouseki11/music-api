5f039c7787df5cf0e4d2e5f5619991cb1ebeaded
kouseki11 <ffadhil1108@gmail.com>
2023-10-19 10:09:49 +0700

    update

--- app.js
+++ app.js
@@ -9,18 +9,20 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Use express.json() for parsing JSON data
 
-// Define a storage engine for multer
+
+// Configure multer to handle file uploads
 const storage = multer.diskStorage({
-  destination: './var/task/uploads', // Define the destination folder for uploaded files
-  filename: function (req, file, cb) {
-    // Generate a unique filename with the .mp3 extension
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Specify the directory where uploaded files will be stored
+  },
+  filename: (req, file, cb) => {
+    // Generate a unique filename for the uploaded file
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, file.fieldname + '-' + uniqueSuffix + '.mp3');
   },
 });
 
-// Initialize multer with the defined storage engine
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 
 
@@ -37,41 +39,47 @@ function saveMusicData() {
   }
   
 
-app.post('/api/music/tracks', upload.single('mp3'), (req, res) => {
-  // Check if there's a file uploaded
-  if (!req.file) {
-    return res.status(400).json({ error: 'Invalid file upload' });
-  }
-
-  const newTrack = req.body;
-
-  if (!newTrack || !newTrack.title) {
-    // Delete the uploaded file if track data is invalid
-    fs.unlinkSync(req.file.path);
-    return res.status(400).json({ error: 'Invalid track data' });
-  }
-
-  // Generate a unique slug based on the track title
-  const slug = newTrack.title.toLowerCase().replace(/\s+/g, '-');
-
-  // Check if the generated slug already exists
-  const existingTrack = musicTracks.find((track) => track.slug === slug);
-
-  if (existingTrack) {
-    // Delete the uploaded file if a track with the same title exists
-    fs.unlinkSync(req.file.path);
-    return res.status(400).json({ error: 'Track with this title already exists' });
-  }
-
-  newTrack.id = (musicTracks.length + 1).toString();
-  newTrack.slug = slug;
-  newTrack.mp3Url = req.file.path; // Store the path to the uploaded MP3 file
-
-  musicTracks.push(newTrack);
-  saveMusicData()
-
-  res.status(201).json({ message: 'Track added', track: newTrack });
-});
+  app.post('/api/music/tracks', upload.single('mp3'), (req, res) => {
+    // Check if there's a file uploaded
+    if (!req.file) {
+      return res.status(400).json({ error: 'Invalid file upload' });
+    }
+  
+    // Get the title from the request body
+    const { title } = req.body;
+  
+    if (!title) {
+      // Delete the uploaded file if track data is invalid
+      fs.unlinkSync(req.file.path);
+      return res.status(400).json({ error: 'Invalid track data - title is missing' });
+    }
+  
+    // Generate a unique slug based on the track title
+    const slug = title.toLowerCase().replace(/\s+/g, '-');
+  
+    // Check if the generated slug already exists
+    const existingTrack = musicTracks.find((track) => track.slug === slug);
+  
+    if (existingTrack) {
+      // Delete the uploaded file if a track with the same title exists
+      fs.unlinkSync(req.file.path);
+      return res.status(400).json({ error: 'Track with this title already exists' });
+    }
+  
+    const newTrack = {
+      id: (musicTracks.length + 1).toString(),
+      title: title,
+      slug: slug,
+      mp3Url: req.file.path, // Store the path to the uploaded MP3 file
+    };
+  
+    musicTracks.push(newTrack);
+  
+    // Call the 'saveMusicData' function to save the data to a JSON file
+    saveMusicData();
+  
+    res.status(201).json({ message: 'Track added', track: newTrack });
+  });
 
 app.get('/api/music/tracks/:slug/mp3', (req, res) => {
     const slug = req.params.slug;
